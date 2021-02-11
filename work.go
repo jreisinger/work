5a920fc680f56282192601ee21f80ea3c57d4613
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-11 21:21:30 +0100

    minor changes in work.go

--- work.go
+++ work.go
@@ -1,7 +1,8 @@
-// Package work concurrently generates and processes tasks. The tasks are
-// generated from lines supplied on STDIN. After each task is processed a result
-// is printed on STDOUT. To use it you just need to implement Factory and Task
-// interfaces.
+// Package work is useful for building CLI tools that need to run many tasks
+// quickly. It concurrently generates and processes tasks. The tasks are
+// generated from lines supplied on STDIN and load balanced among workers for
+// processing. After each task is processed a result is printed on STDOUT. To
+// use it you just need to implement Factory and Task interfaces.
 package work
 
 import (
