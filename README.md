6627a938333d2fd1838ebbe497d990f3b62381c5
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-10-26 20:25:56 +0200

    minor changes in README.md, work.go

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-Package work is useful for building CLI tools that need to run many tasks
-quickly. It concurrently generates and processes tasks. The tasks are
-generated from lines read from file(s) or STDIN and load balanced among
-workers for processing. After each task is processed a result is printed on
-STDOUT.
-
-To use it you just need to implement
-[Factory](https://pkg.go.dev/github.com/jreisinger/work#Factory) and
-[Task](https://pkg.go.dev/github.com/jreisinger/work#Task) interfaces. Then
-Run the Factory. See [examples](examples) folder for sample implementations.
-
-Adapted from John Graham-Cumming's [talk](https://github.com/cloudflare/jgc-talks/tree/master/dotGo/2014).
+See [docs](https://pkg.go.dev/github.com/jreisinger/work) and sample
+implementations in examples directory.
--- work.go
+++ work.go
@@ -1,13 +1,8 @@
 // Package work is useful for building CLI tools that need to run many tasks
-// quickly. It concurrently generates and processes tasks. The tasks are
-// generated from lines read from file(s) or STDIN and load balanced among
-// workers for processing. After each task is processed a result is printed on
-// STDOUT.
-//
-// To use it you just need to implement Factory and Task interfaces. See
-// examples folder for sample implementations. Adapted from John
-// Graham-Cumming's talk:
-// https://github.com/cloudflare/jgc-talks/tree/master/dotGo/2014
+// concurrently. To use it implement Factory and Task interfaces. Then Run the
+// Factory.
+
+// Adapted from https://github.com/cloudflare/jgc-talks/tree/master/dotGo/2014
 package work
 
 import (
@@ -17,23 +12,11 @@ import (
 	"sync"
 )
 
-// Factory generates tasks from lines read from file(s) or STDIN.
-type Factory interface {
-	Generate(line string) Task
-}
-
-// Task is anything that can be processed. The result of the processing is then
-// printed on STDOUT.
-type Task interface {
-	Process()
-	Print()
-}
-
-// Run concurrently runs Factory and nWorkers. Factory generates tasks from
-// lines read from filenames or from STDIN if filenames is empty. The tasks are
-// load balanced among workers that process them. When all tasks are processed
-// the results are printed on STDOUT.
-func Run(f Factory, nWorkers int, filenames []string) {
+// Run concurrently runs Factory and workers. Factory generates tasks from lines
+// read from filenames or from STDIN if filenames is empty. The tasks are load
+// balanced among workers that process them. When all tasks are processed the
+// results are printed on STDOUT.
+func Run(f Factory, workers int, filenames []string) {
 	var wg sync.WaitGroup
 	in := make(chan Task)
 	out := make(chan Task)
@@ -44,7 +27,7 @@ func Run(f Factory, nWorkers int, filenames []string) {
 		defer wg.Done()
 		defer close(in)
 		if len(filenames) == 0 {
-			if err := genTasks(os.Stdin, f, in); err != nil {
+			if err := genTask(os.Stdin, f, in); err != nil {
 				log.Println(err)
 			}
 		} else {
@@ -55,7 +38,7 @@ func Run(f Factory, nWorkers int, filenames []string) {
 					continue
 				}
 				defer file.Close()
-				if err := genTasks(file, f, in); err != nil {
+				if err := genTask(file, f, in); err != nil {
 					log.Println(err)
 					continue
 				}
@@ -64,7 +47,7 @@ func Run(f Factory, nWorkers int, filenames []string) {
 	}()
 
 	// Create workers to process the tasks.
-	for i := 0; i < nWorkers; i++ {
+	for i := 0; i < workers; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -85,12 +68,23 @@ func Run(f Factory, nWorkers int, filenames []string) {
 	}
 }
 
-// getTasks generate Tasks from file and send the to in channel. It uses factory
-// to generate them.
-func genTasks(file *os.File, factory Factory, in chan Task) error {
+// getTask generates Task from file and send it to in channel. It uses factory
+// to generate the Task.
+func genTask(file *os.File, factory Factory, in chan Task) error {
 	s := bufio.NewScanner(file)
 	for s.Scan() {
 		in <- factory.Generate(s.Text())
 	}
 	return s.Err()
+}
+
+// Factory generates task from line read from file or from STDIN.
+type Factory interface {
+	Generate(line string) Task
+}
+
+// Task is anything that can be processed and its result printed on STDOUT.
+type Task interface {
+	Process()
+	Print()
 }
